f1ff85d2d75ae16d216a922f2c8c192d47fca5db
Davide Borsatto <davide.borsatto@gmail.com>
2017-08-17 20:45:08 +0200

    Extracts mappers out of ResourceBuilder

--- src/Management/Role/Constraint/EqualityConstraint.php
+++ src/Management/Role/Constraint/EqualityConstraint.php
@@ -12,7 +12,7 @@ namespace Contentful\Management\Role\Constraint;
 /**
  * EqualityConstraint class.
  *
- * The EqualityConstraint can be used to compare an entity's field
+ * The EqualityConstraint can be used to compare an resource's field
  * or meta dataagainst a specific value.
  *
  * EqualityConstraints are one of the very basic constraints
